08c5d473a53e871d9e25fb0582ae0b63de9a8612
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-07-11 10:05:15 +0800

    feat: Add component to Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,21 @@
-import { metadata } from '@app/layout'
+import { metadata } from "@app/layout";
+import CreateRoom from "@components/CreateRoom";
+import JoinField from "@components/JoinField";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <section>Home page</section>
       <div className="text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{metadata?.title}</h1>
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-8">
+          {metadata?.title}
+        </h1>
+        <section>
+          <JoinField />
+          or
+          <CreateRoom />
+        </section>
       </div>
     </main>
-  )
+  );
 }
