5e90666882edf47f7fb55f2fd9d78b38f56b0e3d
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-07-28 10:02:03 +0800

    feat: Quiz Model/Schema

---
+++ models/quiz.js
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+
+const quizSchema = new Schema({
+  question: {
+    type: String,
+    unique: [true, "Question already exists!"],
+    required: [true, "Question is required!"],
+  },
+  choices: {
+    type: [String],
+    required: [true, "Choices is required!"],
+  },
+  answer: {
+    type: String,
+    required: [true, "Aswer is required!"],
+  },
+});
+
+const Quiz = models.Quiz || model("quiz", quizSchema);
+
+export default Quiz;
