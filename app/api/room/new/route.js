90a1ebb32102e2e840f1046471b073ea29095fb2
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-01 15:40:04 +0800

    fix: Authenticate user before creating new room

--- app/api/room/new/route.js
+++ app/api/room/new/route.js
@@ -1,4 +1,5 @@
 import Room from "@models/room";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 import nanoId from "nano-id";
 
@@ -7,13 +8,29 @@ export const POST = async (req) => {
 
   try {
     await connectToDB();
+
+    //   Authenticate if user exist
+    const creator = await User.findById(creatorId);
+    if (!creator) {
+      return new Response(JSON.stringify({ error: "Unauthorized user" }), {
+        status: 401,
+      });
+    }
+
+    // Create new room environment
     const newRoom = new Room({ creator: creatorId, name, code: nanoId(13) });
     await newRoom.save();
     const { code } = newRoom;
-    
+
+    // Return the code of newly created room
     return new Response(JSON.stringify({ code }), { status: 201 });
   } catch (error) {
     console.log(error.message);
-    return new Response("Failed to create room", { status: 500 });
+    return new Response(
+      JSON.stringify({ error: "Server error! Failed to create room" }),
+      {
+        status: 500,
+      }
+    );
   }
 };
