62d0b9729eb9f7378e1bdd76bf244e6295ca3775
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-02 03:05:13 +0800

    fix: Catch error for invalid id

--- app/api/room/[id]/route.js
+++ app/api/room/[id]/route.js
@@ -30,8 +30,18 @@ export const GET = async (req, { params }) => {
 
     return new Response(JSON.stringify(room), { status: 201 });
   } catch (error) {
-    console.log(error.message);
-    return new Response("Internal Server Error", { status: 500 });
+    console.error(error.message);
+    if (error.name === "CastError" && error.path === "_id") {
+      return new Response(
+        JSON.stringify({ error: "Invalid room id provided" }),
+        {
+          status: 400,
+        }
+      );
+    }
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 };
 
@@ -64,7 +74,17 @@ export const PATCH = async (req, { params }) => {
 
     return new Response(JSON.stringify(room), { status: 200 });
   } catch (error) {
-    console.log(error.message);
-    return new Response("Internal Server Error", { status: 500 });
+    console.error(error.message);
+    if (error.name === "CastError" && error.path === "_id") {
+      return new Response(
+        JSON.stringify({ error: "Invalid room id provided" }),
+        {
+          status: 400,
+        }
+      );
+    }
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 };
