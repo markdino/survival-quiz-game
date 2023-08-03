e4a6bc93b4fe8b1eb0b2d7b1819687ddca2932b1
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-04 01:17:22 +0800

    feat: The rest of functions for API calls

--- services/api.js
+++ services/api.js
@@ -37,3 +37,109 @@ export const signInUser = ({
       onFailed(response);
     });
 };
+
+export const createRoom = ({
+    name = "",
+    creatorId,
+    onSubmit = () => {},
+    onSuccess = () => {},
+    onFailed = () => {},
+  }) => {
+    onSubmit();
+    axios
+      .post(`/api/room/new`, { name, creatorId})
+      .then(({ data }) => {
+        onSuccess(data);
+      })
+      .catch(({ response }) => {
+        onFailed(response);
+      });
+  };
+
+export const getRoomData = ({
+    code,
+    onSubmit = () => {},
+    onSuccess = () => {},
+    onFailed = () => {},
+  }) => {
+    onSubmit();
+    axios
+      .get(`/api/room/${code}`)
+      .then(({ data }) => {
+        onSuccess(data);
+      })
+      .catch(({ response }) => {
+        onFailed(response);
+      });
+  };
+
+  export const getQuiz = ({
+    exclude = [],
+    onSubmit = () => {},
+    onSuccess = () => {},
+    onFailed = () => {},
+  }) => {
+    onSubmit();
+    axios
+      .get(`/api/quiz?exclude=${exclude.join(",")}`)
+      .then(({ data }) => {
+        onSuccess(data);
+      })
+      .catch(({ response }) => {
+        onFailed(response);
+      });
+  };
+
+  export const revealQuiz = ({
+    id,
+    onSubmit = () => {},
+    onSuccess = () => {},
+    onFailed = () => {},
+  }) => {
+    onSubmit();
+    axios
+      .post(`/api/quiz/reveal`, { id })
+      .then(({ data }) => {
+        onSuccess(data);
+      })
+      .catch(({ response }) => {
+        onFailed(response);
+      });
+  };
+
+export const joinRoom = ({
+  roomId,
+  userId,
+  onSubmit = () => {},
+  onSuccess = () => {},
+  onFailed = () => {},
+}) => {
+  onSubmit();
+  axios
+    .patch(`/api/room/${roomId}/participant`, { user: userId })
+    .then(({ data }) => {
+      onSuccess(data);
+    })
+    .catch(({ response }) => {
+      onFailed(response);
+    });
+};
+
+export const submitAnswer = ({
+  roomId,
+  userId,
+  answer,
+  onSubmit = () => {},
+  onSuccess = () => {},
+  onFailed = () => {},
+}) => {
+  onSubmit();
+  axios
+    .patch(`/api/room/${roomId}/participant`, { user: userId, answer })
+    .then(({ data }) => {
+      onSuccess(data);
+    })
+    .catch(({ response }) => {
+      onFailed(response);
+    });
+};
