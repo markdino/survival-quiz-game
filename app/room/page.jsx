886d59635f7c929e13781e5dc2d95da859b8ed0b
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-03 10:19:43 +0800

    fix: Add LoginForm in CreateRoomPage

--- app/room/page.jsx
+++ app/room/page.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+"use client";
+import LoginForm from "@components/LoginForm";
+import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const CreateRoomPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const session = await getSession();
+      const localUser = localStorage.getItem("user");
+
+      if (session?.user || localUser) {
+        setIsLoggedIn(true);
+      }
+    })();
+  }, []);
   return (
     <div>
+      {isLoggedIn ? (
         <h1 className="text-4xl font-bold">Create Room Page</h1>
+      ) : (
+        <LoginForm
+          label="Sign in as Guest"
+          placeholder="Guest username"
+          onSuccess={() => setIsLoggedIn(true)}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CreateRoomPage
+export default CreateRoomPage;
