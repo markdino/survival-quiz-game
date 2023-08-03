2f185d55a606278b1741171456427ca358e845c9
Kirk Santos <kirkalyn13@gmail.com>
2023-08-03 20:53:40 +0800

    feat: add StartGame component
    Start Game logic to follow

--- components/CreatorView.jsx
+++ components/CreatorView.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PlayerList from "./PlayerList";
+import StartGame from "./StartGame";
 
 // Dummy player data
 const players = [
@@ -19,6 +20,7 @@ const CreatorView = () => {
   return (
     <div>
       <PlayerList players={players}/>
+      <StartGame />
     </div>
   );
 };
