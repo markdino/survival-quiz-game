d2c2b6b2572f78dba9774fb63e5b8a47f630bc9a
Kirk Santos <kirk.santos@sanofi.com>
2023-08-03 20:05:31 +0800

    feat: add list of players

--- components/CreatorView.jsx
+++ components/CreatorView.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import PlayerList from "./PlayerList";
+
+// Dummy player data
+const players = [
+  {id: 1, name: "Tony Stark", isOnline: true},
+  {id: 2, name: "Steve Rogers", isOnline: true},
+  {id: 3, name: "Peter Parker", isOnline: true},
+  {id: 4, name: "Natasha Romanoff", isOnline: true},
+  {id: 5, name: "James Rhodes", isOnline: true},
+  {id: 6, name: "Bruce Banner", isOnline: true},
+  {id: 7, name: "Thor Odinson", isOnline: false},
+  {id: 8, name: "Nick Fury", isOnline: true},
+  {id: 9, name: "Wanda Maximoff", isOnline: false},
+  {id: 10, name: "Sam Wilson", isOnline: true},
+]
 
 const CreatorView = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Creator View</h1>
+      <PlayerList players={players}/>
     </div>
   );
 };
---
+++ components/PlayerList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const PlayerList = ({players}) => {
+    const renderPlayerName = (player) =>  {
+        return <li 
+        key={player.id}
+        className="my-2 mx-8 text-md flex justify-between items-center"
+        >
+            {player.name}
+            <div className={`bg-${player.isOnline ? "green" : "red"}-500 w-4 h-4 rounded-full ml-16`}></div>
+        </li>
+    }
+
+  return (
+    <section className="flex flex-col items-center justify-center 
+        w-1/4 h-1/2 mt-24 mx-8 py-24 absolute top-0 left-0
+        border-4 rounded-md border-black">
+            <h3 className='my-16 text-3xl absolute top-0'>List of Players: </h3>
+            <ul className="w-full p-8 my-4 overflow-auto">
+                {players.map(player => renderPlayerName(player))}
+            </ul>
+    </section>
+  )
+}
+
+export default PlayerList
