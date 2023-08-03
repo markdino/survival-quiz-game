f4141d8c9b9d6a26ba3348f9f51f338a0cfc4019
Kirk Santos <kirk.santos@sanofi.com>
2023-08-03 20:11:44 +0800

    feat: improve player online text display

--- components/PlayerList.jsx
+++ components/PlayerList.jsx
@@ -4,7 +4,7 @@ const PlayerList = ({players}) => {
     const renderPlayerName = (player) =>  {
         return <li 
         key={player.id}
-        className="my-2 mx-8 text-md flex justify-between items-center"
+        className={`my-2 mx-4 text-xl flex justify-between items-center ${!player.isOnline ? "text-gray-300" : null}`}
         >
             {player.name}
             <div className={`bg-${player.isOnline ? "green" : "red"}-500 w-4 h-4 rounded-full ml-16`}></div>
@@ -15,7 +15,7 @@ const PlayerList = ({players}) => {
     <section className="flex flex-col items-center justify-center 
         w-1/4 h-1/2 mt-24 mx-8 py-24 absolute top-0 left-0
         border-4 rounded-md border-black">
-            <h3 className='my-16 text-3xl absolute top-0'>List of Players: </h3>
+            <h3 className='my-16 text-2xl absolute top-0'>List of Players: </h3>
             <ul className="w-full p-8 my-4 overflow-auto">
                 {players.map(player => renderPlayerName(player))}
             </ul>
