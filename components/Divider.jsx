5be0f52dbc6e250b86e9d3f77c0d5250b158c1f8
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-03 10:11:22 +0800

    fix: Divider - className prop and other props

--- components/Divider.jsx
+++ components/Divider.jsx
@@ -1,6 +1,8 @@
-const Divider = ({ text }) => {
+import classNames from "classnames";
+
+const Divider = ({ text, className, ...props }) => {
   return (
-    <div className="relative">
+    <div className={classNames("relative", className)} {...props}>
       {text && (
         <div className="absolute inset-x-0 inset-y-0 flex justify-center items-center">
           <p className="px-5 bg-white text-slate-400 font-normal">{text}</p>
