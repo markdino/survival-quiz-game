7335f5dc8cbe88e32fbf9a50a904b04759a96cd8
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-06 02:53:11 +0800

    feat: Integrate API and functions on start, reveal and next button

--- components/StartGame.jsx
+++ components/StartGame.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import classNames from "classnames";
 
-const StartGame = () => {
-    const handleClick = () => {
-        console.log("Start Game")
-        // Start Game logic here
-    }
-
-    return (
-        <section className="flex flex-col items-center justify-center 
+const StartGame = ({ onClick, disabled }) => {
+  return (
+    <section
+      className="flex flex-col items-center justify-center 
             w-1/4 h-1/3 mx-8 mb-8 absolute bottom-0 left-0
-            border-4 rounded-md border-black">
-                <button
-                onClick={handleClick}
-                className="px-16 py-8 ml-2 text-3xl bg-yellow-400 rounded-lg"
-                >
-                    Start Game
-                </button>
-        </section>
-    )
-}
+            border-4 rounded-md border-black"
+    >
+      <button
+        onClick={onClick}
+        className={classNames(
+          "px-16 py-8 ml-2 text-3xl rounded-lg",
+          disabled ? "bg-gray-200 text-gray-500" : "bg-yellow-400"
+        )}
+        disabled={disabled}
+      >
+        Start Game
+      </button>
+    </section>
+  );
+};
 
-export default StartGame
+export default StartGame;
