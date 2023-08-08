a5a1fa6ced03e3d79a0907e08b187035312892b1
Michael Jasper Pabellano <mpabellano@crescendocollective.com>
2023-08-08 21:22:38 +0800

    Add function

--- components/RevealChoices.jsx
+++ components/RevealChoices.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import LargeButton from "./LargeButton";
 
-const RevealChoices = ({ onClick, disabled }) => {
+const RevealChoices = ({ onClickRevealChoice, onClickStartTimer, disabledRevealChoice, disabledStartTimer }) => {
   return (
     <section
       className="flex flex-col justify-content: center items-center mx-60 absolute bottom-0 mb-8
             border-4 rounded-md border-black"
     >
       <div className="h-1/2 m-20 flex flex-row justify-between items-center">
-      <LargeButton onClick={onClick} disabled={disabled}>
+      <LargeButton onClick={onClickRevealChoice} disabled={disabledRevealChoice}>
         Reveal Choices
       </LargeButton>
-      <LargeButton onClick={onClick} disabled={disabled}>
+      <LargeButton onClick={onClickStartTimer} disabled={disabledStartTimer}>
         Start Timer
       </LargeButton>
       </div>
