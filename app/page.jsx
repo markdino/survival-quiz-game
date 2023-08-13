e7a45f1d581229bb58f5bdce7c9c3f4c6398748e
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-13 23:31:48 +0800

    test Prod: Disable goRedirect after signin

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import JoinField from "@components/JoinField";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <section>Home page</section>
+      {/* <section>Home page</section> */}
       <div className="text-center">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-8">
           {metadata?.title}
