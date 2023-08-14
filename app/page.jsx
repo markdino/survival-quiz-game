b717165318c9570be786957e09e8015710ec70fe
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-14 20:10:32 +0800

    feat: Style Home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,20 +2,36 @@ import { metadata } from "@app/layout";
 import CreateRoomButton from "@components/CreateRoomButton";
 import Divider from "@components/Divider";
 import JoinField from "@components/JoinField";
+import mainBg from "@assets/images/main-bg.jpg";
+import Glass from "@components/Glass";
+import Divider2 from "@components/Divider2";
 
 export default function Home() {
+  const mainStyle = {
+    backgroundImage: `url('${mainBg.src}')`,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main
+      style={mainStyle}
+      className="flex min_h_occupied flex-col items-center p-24"
+    >
       {/* <section>Home page</section> */}
       <div className="text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-8">
-          {metadata?.title}
-        </h1>
-        <section>
-          <JoinField />
-          <Divider text="or" className="max-w-md mx-auto" />
-          <CreateRoomButton />
+        <section className="py-6 px-8 border-8 rounded-2xl border-yellow-950 border-double bg-yellow-700 mb-8">
+          <h1
+            className="text-4xl font-bold tracking-tight text-slate-100 sm:text-6xl text_stroke"
+            // style={{ color: "#513493" }}
+          >
+            {metadata?.title}
+          </h1>
         </section>
+        <Glass className="py-20">
+          <JoinField />
+          <Divider2 text="or" className="max-w-md mx-auto py-4" />
+          <CreateRoomButton />
+        </Glass>
       </div>
     </main>
   );
