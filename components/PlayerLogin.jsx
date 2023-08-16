ee9385f31de8a54f9d28807b53f984688fdca29d
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-16 17:53:20 +0800

    fix: Incapsulate Glass inside logic

--- components/PlayerLogin.jsx
+++ components/PlayerLogin.jsx
@@ -7,31 +7,39 @@ import { GAME_TOPIC } from "@websocket/topics";
 import Glass from "./Glass";
 
 const PlayerLogin = ({ roomId }) => {
-
   const { isLoggedIn, setRequestFetch, user, isChecking } =
     useContext(UserContext);
   const socket = useContext(SocketContext);
 
   return (
-    <Glass opacity={0.5} className="flex items-center justify-center mt-32 p-16 absolute top-0">
+    <>
       {!isLoggedIn ? (
-        <LoginForm
-          label="Input your Username"
-          placeholder="Username"
-          onSuccess={(user) => {
-            joinRoom({
-              roomId,
-              userId: user._id,
-              onSuccess: () => {
-                setRequestFetch(true);
-                socket.emit(GAME_TOPIC, { creatorRequestFetch: true, roomId });
-              },
-              onFailed: (error) => {
-                console.log({ error });
-              },
-            });
-          }}
-        />
+        <Glass
+          opacity={0.5}
+          className="flex items-center justify-center mt-32 p-16 absolute top-0"
+        >
+          <LoginForm
+            label="Input your Username"
+            placeholder="Username"
+            onSuccess={(user) => {
+              joinRoom({
+                roomId,
+                userId: user._id,
+                onSuccess: () => {
+                  setRequestFetch(true);
+                  socket.emit(GAME_TOPIC, {
+                    creatorRequestFetch: true,
+                    roomId,
+                  });
+                  console.log("join success", { roomId });
+                },
+                onFailed: (error) => {
+                  console.log({ error });
+                },
+              });
+            }}
+          />
+        </Glass>
       ) : (
         !isChecking &&
         joinRoom({
@@ -46,7 +54,7 @@ const PlayerLogin = ({ roomId }) => {
           },
         })
       )}
-    </Glass>
+    </>
   );
 };
 
