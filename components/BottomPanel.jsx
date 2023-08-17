c55fe2fe4853f653125ffb78816ffa3ef4e906f4
Mark Dino Pelonia <markdinopelonia447@gmail.com>
2023-08-17 15:38:24 +0800

    fix: BottomPanel reduce height

--- components/BottomPanel.jsx
+++ components/BottomPanel.jsx
@@ -5,7 +5,7 @@ const BottomPanel = ({ children}) => {
     <>
       <Glass
       opacity={0.3}
-        className="w-2/5 h-1/4 mb-8 absolute bottom-0"
+        className="w-2/5 h-1/5 mb-8 absolute bottom-0"
       >
         <div className="w-full h-full px-16 flex justify-between items-center">
           {children}
